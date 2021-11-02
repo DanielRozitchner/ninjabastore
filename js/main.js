d64af590845ef632f5f58eb6d8d253e59be95cb2
Daniel Rozitchner <danirozitchner@gmail.com>
2021-11-02 11:52:41 -0300

    cosas

--- js/main.js
+++ js/main.js
@@ -1,93 +1,103 @@
-class Producto {
-    constructor (modelo, talle, color, precio) {
-        this.modelo = modelo;
-        this.talle = talle;
-        this.color = color;
-        this.precio = precio;
-        this.venta = false;
+// class Producto {
+//     constructor (modelo, talle, color, precio) {
+//         this.modelo = modelo;
+//         this.talle = talle;
+//         this.color = color;
+//         this.precio = precio;
+//         this.venta = false;
     
-    }
-    sumarIva() { 
-        this.precio = this.precio * 1.21; 
-    }
-    vender() {
-        this.venta = true;
-        }   
-}
+//     }
+//     sumarIva() { 
+//         this.precio = this.precio * 1.21; 
+//     }
+//     vender() {
+//         this.venta = true;
+//         }   
+// }
 
 
 
 
 
-const listOfProducts = [];
-let salida
-do{
-    let selectModel = prompt("Ingrese el nombre del modelo seleccionado");
-    let selectTalle = prompt("Ingrese S - M - L para elegri el talle");
-    let selectColor = prompt("Ingrese violeta o negro para elegri el color"); 
-    let selectPrecio = parseFloat(prompt("Ingrese el precio del producto seleccionado"));
-    listOfProducts.push(new Producto(selectModel, selectTalle, selectColor, selectPrecio));
-    salida = prompt("Presione X para salir al carrito o cualquier otra tecla para continuar comprando").toLowerCase();
-    }while (salida != "x");
+// const listOfProducts = [];
+// let salida
+// do{
+//     let selectModel = prompt("Ingrese el nombre del modelo seleccionado");
+//     let selectTalle = prompt("Ingrese S - M - L para elegri el talle");
+//     let selectColor = prompt("Ingrese violeta o negro para elegri el color"); 
+//     let selectPrecio = parseFloat(prompt("Ingrese el precio del producto seleccionado"));
+//     listOfProducts.push(new Producto(selectModel, selectTalle, selectColor, selectPrecio));
+//     salida = prompt("Presione X para salir al carrito o cualquier otra tecla para continuar comprando").toLowerCase();
+//     }while (salida != "x");
 
-    for (let i = 0; i < listOfProducts.length; i++) {
-        listOfProducts[i].vender();
-        listOfProducts[i].sumarIva();
+//     for (let i = 0; i < listOfProducts.length; i++) {
+//         listOfProducts[i].vender();
+//         listOfProducts[i].sumarIva();
         
-    }
+//     }
 //storage       
-const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+// const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
 
-guardarLocal("listaProductos", JSON.stringify(listOfProducts));
+// guardarLocal("listaProductos", JSON.stringify(listOfProducts));
 
-const total = []; //declaro array vacio para el precio total
+// const total = []; //declaro array vacio para el precio total
 
-//variables para generar tabla con for of
-let tabla = document.createElement("table");
-tabla.setAttribute("class", "table table-striped table-dark");//bootstrap clases
-let tablaBody = document.createElement("tbody");
-let tablaHead = document.createElement("thead");
-tablaHead.innerHTML = `<th>Modelo</th>
-                        <th>Talle</th>
-                        <th>Color</th>
-                        <th>Precio + IVA</th>`;
-
-
-for (const producto of listOfProducts) {
+// //variables para generar tabla con for of
+// let tabla = document.createElement("table");
+// tabla.setAttribute("class", "table table-striped table-dark");//bootstrap clases
+// let tablaBody = document.createElement("tbody");
+// let tablaHead = document.createElement("thead");
+// tablaHead.innerHTML = `<th>Modelo</th>
+//                         <th>Talle</th>
+//                         <th>Color</th>
+//                         <th>Precio + IVA</th>`;
 
 
-    let fila = document.createElement("tr");
-    fila.innerHTML =       `<td>${producto.modelo}</td>
-                            <td>${producto.talle}</td>
-                            <td>${producto.color}</td>
-                            <td>${producto.precio.toFixed(2)}</td>`;
-    tablaBody.appendChild(fila);
+// for (const producto of listOfProducts) {
+
+
+//     let fila = document.createElement("tr");
+//     fila.innerHTML =       `<td>${producto.modelo}</td>
+//                             <td>${producto.talle}</td>
+//                             <td>${producto.color}</td>
+//                             <td>${producto.precio.toFixed(2)}</td>`;
+//     tablaBody.appendChild(fila);
     
-    total.push(producto.precio);//reutilzando for para pushear precios al array "total"
-}
-//for para precio total del array
-let sum = 0;
-for (let i = 0; i < total.length; i++) {
-    sum += parseFloat(total[i]);
-}
+//     total.push(producto.precio);//reutilzando for para pushear precios al array "total"
+// }
+// //for para precio total del array
+// let sum = 0;
+// for (let i = 0; i < total.length; i++) {
+//     sum += parseFloat(total[i]);
+// }
 
 
-tabla.appendChild(tablaHead);
-tabla.appendChild(tablaBody);
-document.body.appendChild(tabla); 
+// tabla.appendChild(tablaHead);
+// tabla.appendChild(tablaBody);
+// document.body.appendChild(tabla); 
 
-let totalPrice = document.createElement("div");
-totalPrice.setAttribute("class", "d-flex flex-column align-items-center");//bootstrap clases
-totalPrice.innerHTML = `<h3>Total: ${sum.toFixed(2)}</h3>`;
-document.body.appendChild(totalPrice);
+// let totalPrice = document.createElement("div");
+// totalPrice.setAttribute("class", "d-flex flex-column align-items-center");//bootstrap clases
+// totalPrice.innerHTML = `<h3>Total: ${sum.toFixed(2)}</h3>`;
+// document.body.appendChild(totalPrice);
 
 
 //choose event ejercicio 9
 let selectDetails = document.getElementById("selectDetails");
 selectDetails.addEventListener("click", clickSelect)
+let clicked = null;
 function clickSelect () {
-        let selectShirt = document.createElement("select");
-        selectShirt.innerHTML = `<option value="negro">Negro</option>
-                                <option value="Lila">Lila</option>`;
-     document.getElementById("selector").appendChild(selectShirt);
+    if (clicked == null) {$("#selector").append(`<select id="color"><option value=""></option><option value="negro">Negro</option>
+    <option value="Lila">Lila</option></select><select><option value="L">Large</option>
+                            <option value="M">Medium</option></select>
+                            <input type="submit"></input>`);
+clicked = 1;        
+}
+}
+
+let colorSelect= document.getElementById("color");
+
+function colorElegido() {
+    productos[0].color = (colorSelect.value);
+    console.log(productos[0]);
 }
---
+++ js/productos.js
@@ -0,0 +1,51 @@
+const productos = [{
+    modelo: "Remera Ninja",
+    talle: "",
+    color: "",
+    precio: 1750
+},
+{
+    modelo: "Marge",
+    talle: "",
+    color: "",
+    precio: 1960
+},
+{
+    modelo: "Joey",
+    talle: "",
+    color: "",
+    precio: 2100,
+},
+{
+    modelo: "The Office",
+    talle: "",
+    color: "",
+    precio: 2300
+}]
+
+
+// let empresa = document.getElementById("empresa");
+
+// let stock = document.getElementById("cantidad");
+
+// //! EVENTOS
+
+// // let botonIngresarDatos = document.getElementById("boton-ingresar-datos");
+
+// // botonIngresarDatos.addEventListener("click", ingresarDatos);
+
+// // function ingresarDatos() {
+// // productos[0].talle = (empresa.value);
+// // productos[0].color = (stock.value);
+
+// // console.log(productos);
+// // }
+
+
+// // <div class="product-device product-device-2 shadow-sm d-none d-md-block">
+// //             <label>EMPRESA:</label>
+// //             <input id="empresa" type="text" placeholder="aqui nombre de la Empresa">
+// //             <label>CANTIDAD DE STOCK</label>
+// //             <input id="cantidad" type="number" placeholder="aqui cantidad de stock">
+// //             <input id="boton-ingresar-datos" type="submit" value="Ingresar Valor">
+// //         </div>
