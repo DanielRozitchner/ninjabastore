d39bfca0757c4774d59321df859c700872edc18e
Daniel Rozitchner <danirozitchner@gmail.com>
2021-11-02 18:36:27 -0300

    casi casi

--- js/main.js
+++ js/main.js
@@ -83,21 +83,119 @@
 
 
 //choose event ejercicio 9
-let selectDetails = document.getElementById("selectDetails");
-selectDetails.addEventListener("click", clickSelect)
-let clicked = null;
-function clickSelect () {
-    if (clicked == null) {$("#selector").append(`<select id="color"><option value=""></option><option value="negro">Negro</option>
-    <option value="Lila">Lila</option></select><select><option value="L">Large</option>
-                            <option value="M">Medium</option></select>
-                            <input type="submit"></input>`);
-clicked = 1;        
-}
-}
+// let selectDetails = document.getElementById("selectDetails");
+// selectDetails.addEventListener("click", clickSelect)
+// let clicked = null;
+// function clickSelect () {
+//     if (clicked == null) {$("#selector").append(`<form>
+//     <label for="seleccionarTalle">Selecciona el talle y el color</label>
+//     <select id="seleccionarTalle">
+//         <option value="S">S</option>
+//         <option value="M">M</option>
+//         <option value="L">L</option>
+//         <option value="XL">XL</option>
+//     </select>
+//     <button id="btn">Confirmar Seleccion</button>
+// </form>`);
+// const btn = $('#btn');
+// const sT = $('#seleccionarTalle')
+// btn.onclick = (event) => {
+//     event.preventDefault();
+    
+//     console.log(sT.value);
+// };
+// clicked = 1; 
+// }
+// }
 
-let colorSelect= document.getElementById("color");
 
-function colorElegido() {
-    productos[0].color = (colorSelect.value);
-    console.log(productos[0]);
-}
+// let colorSelect= document.getElementById("color");
+
+// function colorElegido() {
+//     productos[0].color = (colorSelect.value);
+//     console.log(productos[0]);
+// }
+
+// const agregarAlCarrito = event => {
+//     // arrayCarrito.push(productoAgregado)
+//     console.log(event.target)
+// };
+// productos.forEach(newProduct => {
+//     const producto = `
+//         <div class="producto">
+//             <h3 class="producto__modelo">${newProduct.modelo}</h3>
+//             <div><img src="${newProduct.img}"></div>     
+//             <select>${newProduct.talle.map(talle => `<option value="${talle}">${talle}</option>`)}</select>
+//             <select>${newProduct.color.map(color => `<option value="${color}">${color}</option>`)}</select>
+//             <h3 class="producto__precio">${newProduct.precio}</h3>
+//             <button class="car">Agregar al carrito</button>
+//         </div>
+//           `;
+//       $('#cards').append(producto);
+//     });
+//     document.querySelectorAll(".car").forEach(btn => btn.addEventListener("click", event => {
+//         debugger
+//         const precio = event.target.previousElementSibling.innerText;
+//         const color = event.target.previousSibling.previousSibling.value;
+//         const talle = event.target.previousSibling.previousSibling.previousSibling.value;
+//         const img = event.target.previousSibling.previousSibling.previousSibling.previousSibling.src;
+//         const modelo = event.target.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.innerText;
+        
+//         const productoSeleccionado = {
+//             precio,
+//             color,
+//             talle,
+//             img,
+//             modelo
+//         };
+//         arrayCarrito.push(productoSeleccionado);
+//         console.log(arrayCarrito);
+
+//     }));
+
+// $(".car").forEach(btn => $(btn).onclick(event => console.log(event)));
+// console.log(arrayCarrito);
+
+
+const agregarAlCarrito = productoSeleccionado => {
+    const productoAgregado = {
+        ...productoSeleccionado, // spread operator
+        talle: document.querySelector(`#talleSeleccionado-${productoSeleccionado.id}`).value,
+        color: document.querySelector(`#colorSeleccionado-${productoSeleccionado.id}`).value
+    }
+    arrayCarrito.push(productoAgregado);
+    console.log(arrayCarrito);
+
+    let almacenados;
+     
+     if (JSON.parse(localStorage.getItem("listaProductos")).length) {
+        almacenados = JSON.parse(localStorage.getItem("listaProductos"));
+     } else {
+         almacenados= []
+     }
+     almacenados.push(productoAgregado)
+    
+    localStorage.setItem("listaProductos",JSON.stringify(almacenados));
+    
+    // = JSON.parse(localStorage.getItem("listaProductos"));
+Swal.fire({
+  position: 'top-end',
+  icon: 'success',
+  title: 'Nuevo producto agregado al carrito',
+  showConfirmButton: false,
+  timer: 1300
+})
+};
+productos.forEach(newProduct => {
+    const producto = `
+        <div class="producto">
+            <h3 class="producto__modelo">${newProduct.modelo}</h3>
+            <div><img src="${newProduct.img}"></div>     
+            <select id="talleSeleccionado-${newProduct.id}">${newProduct.talle.map(talle => `<option value="${talle}">${talle}</option>`)}</select>
+            <select id="colorSeleccionado-${newProduct.id}">${newProduct.color.map(color => `<option value="${color}">${color}</option>`)}</select>
+            <h3 class="producto__precio">$  ${newProduct.precio}</h3>
+            <button onclick='agregarAlCarrito(${JSON.stringify(newProduct)});'>Agregar al carrito</button>
+        </div>
+          `;
+      document.querySelector("#cards").innerHTML += producto;
+    });
--- js/productos.js
+++ js/productos.js
@@ -1,25 +1,36 @@
+const posiblesTalles = ["S","M","L","XL"];
+const posiblesColores = ["Lila","Negro"];
+const arrayCarrito = [];
 const productos = [{
+    id: "1",
     modelo: "Remera Ninja",
-    talle: "",
-    color: "",
+    img: "../assets/WhatsApp Image 2021-10-14 at 13.57.06 (1).jpeg",
+    talle: posiblesTalles,
+    color: posiblesColores,
     precio: 1750
 },
 {
+    id: "2",
     modelo: "Marge",
-    talle: "",
-    color: "",
+    img:"../assets/WhatsApp Image 2021-10-14 at 13.57.06 (2).jpeg",
+    talle: posiblesTalles,
+    color: posiblesColores,
     precio: 1960
 },
 {
+    id: "3",
     modelo: "Joey",
-    talle: "",
-    color: "",
-    precio: 2100,
+    img: "../assets/WhatsApp Image 2021-10-14 at 13.57.07.jpeg",
+    talle: posiblesTalles,
+    color: posiblesColores,
+    precio: 2100
 },
 {
+    id: "4",
     modelo: "The Office",
-    talle: "",
-    color: "",
+    img: "../assets/WhatsApp Image 2021-10-14 at 13.57.07 (1).jpeg",
+    talle: posiblesTalles,
+    color: posiblesColores,
     precio: 2300
 }]
 
