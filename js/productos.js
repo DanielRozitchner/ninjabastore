b92b9e38bc0f3f9593fa5f7087061471791d41c6
Daniel Rozitchner <danirozitchner@gmail.com>
2021-11-03 01:33:25 -0300

    problema fotos

--- js/productos.js
+++ js/productos.js
@@ -4,7 +4,7 @@ const arrayCarrito = [];
 const productos = [{
     id: "1",
     modelo: "Remera Ninja",
-    img: "../assets/WhatsApp Image 2021-10-14 at 13.57.06 (1).jpeg",
+    img: "assets/WhatsApp Image 2021-10-14 at 13.57.06 (1).jpeg",
     talle: posiblesTalles,
     color: posiblesColores,
     precio: 1750
@@ -12,7 +12,7 @@ const productos = [{
 {
     id: "2",
     modelo: "Marge",
-    img:"../assets/WhatsApp Image 2021-10-14 at 13.57.06 (2).jpeg",
+    img:"assets/WhatsApp Image 2021-10-14 at 13.57.06 (2).jpeg",
     talle: posiblesTalles,
     color: posiblesColores,
     precio: 1960
@@ -20,7 +20,7 @@ const productos = [{
 {
     id: "3",
     modelo: "Joey",
-    img: "../assets/WhatsApp Image 2021-10-14 at 13.57.07.jpeg",
+    img: "assets/WhatsApp Image 2021-10-14 at 13.57.07.jpeg",
     talle: posiblesTalles,
     color: posiblesColores,
     precio: 2100
@@ -28,7 +28,7 @@ const productos = [{
 {
     id: "4",
     modelo: "The Office",
-    img: "../assets/WhatsApp Image 2021-10-14 at 13.57.07 (1).jpeg",
+    img: "assets/WhatsApp Image 2021-10-14 at 13.57.07 (1).jpeg",
     talle: posiblesTalles,
     color: posiblesColores,
     precio: 2300
