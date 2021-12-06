d3094f2d9e6ec744e434c9cc34013b4ceaa141d3
Daniel Rozitchner <danirozitchner@gmail.com>
2021-12-06 00:54:02 -0300

    final

--- js/funciones.js
+++ js/funciones.js
@@ -1,27 +1,37 @@
+//funcion eliminar productos del carrito y local storage
 function eliminarEl() {
-    $(".btn-remove").click(function() {
+        $(".btn-remove").click(function() {
+        let atributo = $(this).attr("data-id"); //obteniendo valor de id del producto a borrar
+        let atributoSize = $(this).attr("data-size"); // obteniendo talle del producto a borrar
+        let atributoColor = $(this).attr("data-color"); //obteniendo color del producto a borrar
+        $(this).parent('td').parent('tr').remove(); //borrando producto del dom
+        const laLista = JSON.parse(localStorage.getItem("listaProductos"));
+        //localizando el index del producto a borrar dentro del array del storage segun los valores obtenidos
+        const findId = laLista.findIndex(element => element.id == atributo && element.talle == atributoSize && element.color == atributoColor); 
+        //condicion para evitar error en consola, la funcion se activaba 2 veces
+        if (laLista[findId]){
+        priceFind = laLista[findId].precio;
+        sum = sum - priceFind;
+        $("#total").html(`Total: $${sum.toFixed(2)}`);
+        laLista.splice(findId, 1);
+        arrayCarrito.splice(findId, 1);
+        localStorage.setItem("listaProductos",JSON.stringify(laLista));
     
-    let atributo = $(this).attr("data-id");
-    $(this).parent('td').parent('tr').remove();
-    const laLista = JSON.parse(localStorage.getItem("listaProductos"))
-    const finder = laLista.findIndex(element => element.id === atributo);
-    priceFind = laLista[finder].precio;
-    sum = sum - priceFind;
-    $("#total").html(`Total: $${sum.toFixed(2)}`);
-    laLista.splice(finder, 1);
-    arrayCarrito.splice(finder, 1);
-    localStorage.setItem("listaProductos",JSON.stringify(laLista));
-		
     checkCarrito(arrayCarrito, laLista);
-})
+        }
+    })
 }
-function checkCarrito(listCarrito, storageCarrito) {
+//funcion con condicion para borrar html si no hay nada en el carrito o en el local storage
+    function checkCarrito(listCarrito, storageCarrito) {
     if (!listCarrito.length && !storageCarrito.length) {
         $('.total').empty();
         $(".login").empty();
+        $(".registroModal").empty();
 		}
 }
 
+
+//confirmacion compra/ post de la compra y get simulando comprobante de compra obteniendo el id de la api
 function confirmarCompra() {
 const datosUsuario = {Nombre: $("#valorNombre").val(), Mail: $("#valorMail").val()};
 $('#btnCon').hide();
@@ -56,6 +66,7 @@ $('#btnCon').hide();
     $(".registroModal").empty();
     sum = 0;
 }
+//append de formulario
 function registrarCompra() {
     $(".registroModal").empty();
     $(".registroModal").append(`<form>
@@ -72,14 +83,12 @@ function registrarCompra() {
 			validar();
 		})
 }
-
-// const datosRegistro=[nombre = false, mail = false]
-
+//parametros de validacion de inputs
 const validateExp = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
 	mail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
-
+//validacion de parametros regex con inputs
 function validar () {
 	const valorNombre = $("#valorNombre").val();
 	const valorMail = $("#valorMail").val();
@@ -89,12 +98,11 @@ function validar () {
 		Swal.fire({
 				icon: 'error',
 				title: 'Por favor, completar todos los datos',
-				timer: 3000
 		})
 	} else {  
 		Swal.fire({
 				icon: 'error',
-				title: 'Datos incorrectos'
+				title: 'Datos incorrectos',
 		})
 	}
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let sum = 0;
-
+//obteniendo productos del local storage para pintar en el carrito
 $(document).ready(function() {
     if ("listaProductos" in localStorage) {
         const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
@@ -11,19 +11,19 @@ $(document).ready(function() {
                     <td>${almacen.modelo}</td>
                     <td>${almacen.talle}</td>
                     <td>${almacen.color}</td>
-                    <td class='price-market'>$${almacen.precio}</td>
+                    <td class='price-market-${almacen.id}-${almacen.talle}-${almacen.color}'>$${almacen.precio}</td>
                     <td>
-                        <input type="number" class='qty-market' value=${almacen.cantidad} style="width: 2em"></input>
+                        <input class='qty-market-${almacen.id}-${almacen.talle}-${almacen.color}' value=${almacen.cantidad} style="width: 2em"></input>
                     </td>
                     <td>
-                        <button data-id= ${almacen.id}  class= "btn btn-remove btn-danger btn-small">
+                        <button data-id= ${almacen.id} data-size= ${almacen.talle} data-color= ${almacen.color}   class= "btn btn-remove btn-danger btn-small">
                             <i class="bi bi-trash-fill"></i>
                         </button>
                     </td>
                 </tr>`)
                     sum += parseFloat(almacen.precio);
             } 
-             eliminarEl();
+            eliminarEl();
             $(".total").append(`<h3 id="total">Total: $${sum.toFixed(2)}</h3>`)
             $(".login").append(`<button class="btn btn-danger btn-modal" id="btn-login">Registrar Compra</button>
             `)
@@ -34,11 +34,9 @@ $(document).ready(function() {
 });
 
 let cantidad;
-let locatePos;
-let sizeLocation;
-let colorLocation;
 
 
+//funcion para agregar productos al carrito
 const agregarAlCarrito = productoSeleccionado => {
     const productoAgregado = {
         ...productoSeleccionado, // spread operator
@@ -46,19 +44,19 @@ const agregarAlCarrito = productoSeleccionado => {
         color: document.querySelector(`#colorSeleccionado-${productoSeleccionado.id}`).value
     }
     const laLista = JSON.parse(localStorage.getItem("listaProductos"));
-    
+    //unificando storage con arrayCarrito
     if (!arrayCarrito.length && laLista){
         arrayCarrito = laLista;
     }
-
+//localizando producto en el array para usar en if else
     let located = arrayCarrito.filter(prod => prod.id == productoAgregado.id && prod.talle == productoAgregado.talle && prod.color == productoAgregado.color);
-debugger;
+// condicion para pintar agregar si no encuentra producto con mismas caracteristicas
     if (!located.length || productoAgregado.talle != located[0].talle || productoAgregado.color != located[0].color)  {
         
         arrayCarrito.push(productoAgregado);
     
         let almacenados;
-
+//chequeando storage para sumar producto al storage
         if (JSON.parse(localStorage.getItem("listaProductos"))) {
         almacenados = JSON.parse(localStorage.getItem("listaProductos"));
 
@@ -86,18 +84,16 @@ debugger;
         <td>${productoAgregado.talle}</td>
         <td>${productoAgregado.color}</td>
         <td class='price-market-${productoAgregado.id}-${productoAgregado.talle}-${productoAgregado.color}' id= "precio-${productoAgregado.id}">$${productoAgregado.precio}</td>
-        <td><input type="number" value=1 class="qty-market-${productoAgregado.id}-${productoAgregado.talle}-${productoAgregado.color}" quant-${productoAgregado.id}" style="width: 2em"></input></td>
+        <td><input value=1 class="qty-market-${productoAgregado.id}-${productoAgregado.talle}-${productoAgregado.color}" quant-${productoAgregado.id}" style="width: 2em"></input></td>
         <td>
-            <button data-id= ${productoAgregado.id}  class= "btn btn-remove btn-danger btn-small ${productoAgregado.id}">
+            <button data-id= ${productoAgregado.id} data-size= ${productoAgregado.talle} data-color= ${productoAgregado.color}  class= "btn btn-remove btn-danger btn-small ${productoAgregado.id}">
                 <i class="bi bi-trash-fill"></i>
             </button>
         </td>
         </tr>`)
     sum += parseFloat(productoAgregado.precio);
-    locatePos = arrayCarrito.findIndex(p => p.id == productoAgregado.id);
-    // sizeLocation = arrayCarrito[locatePos].talle;
-    // colorLocation = arrayCarrito[locatePos].color;
     }
+    //iterando el array para sumar cantidad de productos, precio y al storage
     else{
         for (let i = 0; i < arrayCarrito.length; i++) {
            if (productoAgregado.id == arrayCarrito[i].id && productoAgregado.talle == arrayCarrito[i].talle && productoAgregado.color == arrayCarrito[i].color){ 
@@ -107,14 +103,13 @@ debugger;
             localStorage.setItem("listaProductos", JSON.stringify(arrayCarrito));  
         }
     }
-                // locatePos = arrayCarrito.findIndex(p => p.id == productoAgregado.id);
+   
 
         $(`#precio-${productoAgregado.id}`).text(`$${subPrecio}`);
         
-    //    $(`#quant-${productoAgregado.modelo}`).val(cantidad);
         
         $(`.quant-${productoAgregado.id}`).value = cantidad;
-        $(`.price-market-${productoAgregado.id}-${productoAgregado.talle}-${productoAgregado.color}`).html(subPrecio);
+        $(`.price-market-${productoAgregado.id}-${productoAgregado.talle}-${productoAgregado.color}`).html(`$${subPrecio}`);
         $(`.qty-market-${productoAgregado.id}-${productoAgregado.talle}-${productoAgregado.color}`).val(cantidad);
         sum += parseFloat(productoAgregado.precio);
         
@@ -141,7 +136,7 @@ debugger;
     
 };
 
-
+//cargando los productos de la pagina dinamicamente
 productos.forEach(newProduct => {
             const producto = `
         <div class="col mb-4">  
@@ -167,197 +162,3 @@ productos.forEach(newProduct => {
             $( this ).fadeIn( 500 )
             $( this ).css("color", "blueviolet");
         });
-
-    
-
-
-
-
-    //calcular total para enviar datos a backend    
-    
-    // function calcularTotal() {
-    //     let sum = 0;
-    // for (let i = 0; i < total.length; i++) {
-    //     sum += parseFloat(total[i]);
-    // }
-    // return sum;
-    // }
-    
-
-/* SECCION USUARIO */
-
-// const inputs = ["nombreApellido","mail"]
-// const labels =  ["Nombre y apellido", "Correo"]
-// const places = ["Nombre completo","Ingrese su correo"]
-// /* ACA VAN A IR LOS NUEVOS USUARIOS */
-// let usuarios=[]
-
-// $("#usuarioBoton").click(function(){
-    
-//     for(const inputsNames of inputs){ 
-        
-//         $("#formularioUsuario").append(`
-//         <label for="" id="labelUsuario" class="labelUsuario">
-//             ${labels[i]}           
-//             <input type="text"  placeholder=${places[i]} class="inputUsuario" name = ${inputsNames} id="${inputsNames}"></input>
-//         </label>`)
-//         i = i+1;
-//     }
-//     $("#formularioUsuario").append(`
-//         <button  type="submit" id="registro"> Registrarse </button>
-//         <button type="reset" >Limpiar </button>
-//     `)
-    
-
-
-
-//     const form = document.getElementById("formularioUsuario")
-//     /* Funcion que toma el boton de envio */
-
-//     /* USUARIO REGISTRADO? */
-
-//     let uRegistrado = false
-
-    // form.addEventListener("submit",(e)=>{
-    //     e.preventDefault(); 
-//         if ((inputsLlenados.nombreApellido === true ) && (inputsLlenados.nombreUsuario === true ) && (inputsLlenados.gmail === true) && (inputsLlenados.clave === true ) && (inputsLlenados.claveVerificada === true )){
-//             swal.fire({
-//                 icon: 'success',
-//                 title: 'Se a registrado con exito!',
-//             });
-//             /* Creacion de usuarios */
-//             usuarios.push(usuarioTerminado)
-//             console.log(usuarios)
-//             localStorage.setItem("usuario",JSON.stringify(usuarios))
-
-            
-
-//             /* resetea el form */
-//             form.reset()
-
-//             uRegistrado = true
-
-//         }else{
-            
-//             swal.fire({
-
-//                 icon: 'error',
-//                 title: 'Rellene los campos faltantes',
-
-//             });
-            
-//         }
-//     })
-
-// })
-
-// /* Expresiones */
-// const expresiones = {
-//     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-// 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-// 	contra: /^.{4,12}$/, // 4 a 12 digitos.
-// 	gmail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-// }
-
-// /* Los inputs estan definidos como false al principio para que no pueda ser enviado el formulario de una */
-// const inputsLlenados=[nombre = false, mail = false]
-// class user {
-//     constructor(nC,nDU,c,c1,c2) {
-//         this.nombreCompleto = nC;
-//         this.nombreDeUsuario = nDU;
-//         this.correo = c;
-//         this.contra1 = c1;
-//         this.contra2 = c2;
-
-//     }
-// }
-
-
-
-// const validarForm = (e) => {
-//     let datosUsuario
-//     switch (e.target.name) {
-//         case "nombreApellido":{
-
-//             validar(expresiones.nombre,e,"nombreApellido",nombreApellido)
-//             let name =e.target.value
-//             console.log(name)
-//             datosUsuario= new user (name,"hola","","","")
-//             usuarios.push(datosUsuario)
-//             break
-//         }
-//         case "gmail":{
-
-//             validar(expresiones.gmail,e,"gmail")
-
-
-
-//             break
-//         }
-//     }
-//             if (clave2.value !== clave1.value ) {
-
-//                 /* Validacion Incorrecta */
-//                 document.getElementById("claveVerificada").classList.remove("formVacio")
-//                 document.getElementById("claveVerificada").classList.remove("formCorrecto")
-//                 document.getElementById("claveVerificada").classList.add("formMal")
-//                 inputsLlenados.claveVerificada = false;
-                    
- 
-//             }else if (clave2.value == " ") {
-//                 /* Validacio Vacia */
-//                 console.log("Estoy vacio")
-//                 document.getElementById("claveVerificada").classList.remove("formCorrecto")
-//                 document.getElementById("claveVerificada").classList.remove("formMal")
-//                 document.getElementById("claveVerificada").classList.add("formVacio")
-//                 inputsLlenados.claveVerificada = false;
-                
-
-//             }else if (clave2.value === clave1.value ){
-
-//                 /* Validacion correcta */
-//                 document.getElementById("claveVerificada").classList.remove("formMal")
-//                 document.getElementById("claveVerificada").classList.remove("formVacio")
-//                 document.getElementById("claveVerificada").classList.add("formCorrecto")
-//                 inputsLlenados.claveVerificada = true;
-//             }
-
-//             break
-
-//     }
-//     console.log(datosUsuario)
-//     console.log(usuarios)
-
-
-
-
-
-// /* FUNCION DE VALIDACION */
-
-// function validar (expresion,evento,campos){
-//     if (expresion.test(evento.target.value)){
-//         /* Validacion correcta */
-
-//         document.getElementById(campos).classList.remove("formMal");
-//         document.getElementById(campos).classList.remove("formVacio");
-//         document.getElementById(campos).classList.add("formCorrecto");
-//         inputsLlenados[campos] = true;       
-
-//     }else if (evento.target.value == "") {
-//         /* Validacio Vacia */
-//         console.log("Estoy vacio")
-//         document.getElementById(campos).classList.remove("formCorrecto")
-//         document.getElementById(campos).classList.remove("formMal")
-//         document.getElementById(campos).classList.add("formVacio")
-//         inputsLlenados[campos]=false;
-
-//     } else {
-//         /* Validacion Incorrecta */
-//         document.getElementById(campos).classList.remove("formVacio")
-//         document.getElementById(campos).classList.remove("formCorrecto")
-//         document.getElementById(campos).classList.add("formMal") 
-//         inputsLlenados[campos]=false;
-
-//     }
-// }
-
